4c35406df637b720dca4a41f97f1a814a04503c8
José Galdámez <josegaldamez1991@gmail.com>
2023-03-16 21:09:58 -0600

    Revert "Operaciones de PUT, POST y GET sobre las vistas de Clientes y Paquetes"

--- src/main/java/com/example/application/data/entity/PackageModel.java
+++ src/main/java/com/example/application/data/entity/PackageModel.java
@@ -5,19 +5,27 @@ import javax.persistence.Entity;
 @Entity
 public class PackageModel extends AbstractEntity {
 
-    private Long packageid;
+    private Integer packageid;
+    private String image;
     private String namepackage;
     private String destiny;
+    private Integer duration;
     private String hotel;
     private String activities;
-    
-    public Long getPackageID() {
+    private Integer price;
+
+    public Integer getPackageID() {
         return packageid;
     }
-    public void setPackageID(Long packageID) {
+    public void setPackageID(Integer packageID) {
         this.packageid = packageID;
     }
-   
+    public String getImage() {
+        return image;
+    }
+    public void setImage(String image) {
+        this.image = image;
+    }
     public String getNamePackage() {
         return namepackage;
     }
@@ -30,8 +38,12 @@ public class PackageModel extends AbstractEntity {
     public void setDestiny(String destiny) {
         this.destiny = destiny;
     }
-   
-    
+    public Integer getDuration() {
+        return duration;
+    }
+    public void setDuration(Integer duration) {
+        this.duration = duration;
+    }
     public String getHotel() {
         return hotel;
     }
@@ -44,6 +56,11 @@ public class PackageModel extends AbstractEntity {
     public void setActivities(String activities) {
         this.activities = activities;
     }
-    
+    public Integer getPrice() {
+        return price;
+    }
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
 
 }
--- src/main/java/com/example/application/data/service/DatabaseServicePaquetes.java
+++ src/main/java/com/example/application/data/service/DatabaseServicePaquetes.java
@@ -1,15 +1,9 @@
 package com.example.application.data.service;
-import com.example.application.data.entity.ClientModel;
-import com.example.application.data.entity.PackageModel;
 import com.example.application.data.entity.PaqueteResponse;
 
-import okhttp3.ResponseBody;
 import retrofit2.Call;
-import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
-import retrofit2.http.POST;
-import retrofit2.http.PUT;
 
 public interface DatabaseServicePaquetes {
 	
@@ -17,24 +11,11 @@ public interface DatabaseServicePaquetes {
 	    "Accept: application/json",
 	    "User-Agent: Retrofit-Sample-App"
 	})
+
+	
 	@GET("turiApp/paquetes")
 	Call<PaqueteResponse> listarPaquetes();
 	
-
-
-	@Headers({
-	    "Accept: application/json",
-	    "User-Agent: Retrofit-Sample-App"
-	})
-	@POST("turiApp/paquetes")
-	Call<ResponseBody> crearPaquetes(@Body PackageModel nuevo);
 	
-	
-	@Headers({
-	    "Accept: application/json",
-	    "User-Agent: Retrofit-Sample-App"
-	})
-	@PUT("turiApp/paquetes")
-	Call<ResponseBody> actualizarPaquetes(@Body PackageModel actualizar);
 
 }
